8847bb429b5ac00c1df3dcb26a31c39035a52409
tsabrandon <tsabrandon@gmail.com>
2024-11-13 16:12:44 +0800

    sample auto deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1> Hello welcome to my portfolio</h1>
+      <h1> Hello welcome to my portfolio </h1>
+      <p>sample auto deploy</p>
     </div>
   );
 }
